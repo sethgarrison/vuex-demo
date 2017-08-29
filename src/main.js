28a6670bbdbcec28c16b3b8ab74a65364fb7854f
Seth Garrison <sethg@l4digital.com>
2017-08-29 12:54:56 -0700

    the simplest store

--- src/main.js
+++ src/main.js
@@ -1,17 +1,40 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-import store from './store'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
+
+// you have to do this before creating a store
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  // the state of the store
+  state: {
+    count: 0
+  },
+  // mutations on the store - these have to be synchronous
+  mutations: {
+    // state is a default argument of a mutation
+    increment: state => state.count++,
+    decrement: state => state.count--
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  store,
-  template: '<App/>',
-  components: { App }
+  computed: {
+    count () {
+      return store.state.count
+    }
+  },
+  methods: {
+    increment () {
+      store.commit('increment')
+    },
+    decrement () {
+      store.commit('decrement')
+    }
+  }
 })
